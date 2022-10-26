ce34d17038c57c1cd0b27d4c1e38562fef757d61
wnakano <nknvolpi@gmail.com>
2022-10-26 19:12:31 -0300

    feat: brain dataset scope

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,5 +1,16 @@
 
-from torch.utils.data import Dataset, DataLoader
+from ast import Call
 import nibabel as nib 
+from typing import Sequence, Optional, Callable
+from torch.utils.data import Dataset, DataLoader
 
-#class 
+class BrainDataset(Dataset):
+    def __init__(self, data: Sequence, transforms: Optional[Callable] = None) -> None:
+        pass
+
+    def __getitem__(self, idx) -> Sequence:
+        pass
+
+    def __len__(self) -> int:
+        pass
+        

51e1927a2226ec84f440f05411e2a6675f7c50e6
Fcsalvagnini <felip.eng@outlook.com.br>
2022-11-22 17:29:45 -0300

    Adds notebook to test dali data augmentation

--- src/utils/json/json_handler.py
+++ src/utils/json/json_handler.py
@@ -1,15 +1,12 @@
 import json
 
 from pathlib import Path
+import os
 from typing import Dict
 
-
-class JsonHandler:
-
-    @staticmethod
-    def parse_json_to_dict(phase: str) -> Dict:
-        root_dir = Path.cwd() /  Path("src/data/descriptors")
-        json_path = str(root_dir / Path(f"{phase}.json"))
-        with open(json_path, 'r') as j:
-            parsed_json = json.load(j)
-        return parsed_json
+def parse_json_to_dict(data_descriptors_path:str, phase:str) -> Dict:
+    root_dir = os.path.join(Path.cwd(), Path(data_descriptors_path))
+    json_path = os.path.join(root_dir, Path(f"{phase}.json"))
+    with open(json_path, 'r') as j:
+        parsed_json = json.load(j)
+    return parsed_json
